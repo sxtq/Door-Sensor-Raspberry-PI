3d9b61a65557f3d6cde369038fd9825253857803
sxtq <admin@qubitmail.com>
2021-03-14 22:20:21 +0000

    Update lock.sh

--- lock.sh
+++ lock.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-echo "Sleepting for 2 minutes before starting socat"
-sleep 2m
-echo "Starting socat"
-socat -u tcp-l:35432,fork system:/home/user/working/resp.sh
+while test "$#" -gt 0; do
+  case "$1" in
+    -s|--socat)
+      shift
+        export socatr="1"
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+if [ "$socatr" = "1" ]; then
+  clear
+  echo "Starting socat"
+  socatr=0
+  socat -u tcp-l:35432,fork system:/home/six/working/lock.sh
+else
+  echo "Locking the screen"
+  xdg-screensaver lock
+fi
