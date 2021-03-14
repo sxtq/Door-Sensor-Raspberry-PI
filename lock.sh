5d3b8bb1a681541e77c73254e7077ae34979094e
sxtq <admin@qubitmail.com>
2021-03-14 22:49:31 +0000

    Update lock.sh

--- lock.sh
+++ lock.sh
@@ -2,6 +2,7 @@
 
 dir=/home/user/lock.sh
 port=35432
+key="your otp key here"
 
 while test "$#" -gt 0; do
   case "$1" in
@@ -23,5 +24,14 @@ if [ "$socatr" = "1" ]; then
   socat -u tcp-l:"$port",fork system:"$dir"
 else
   echo "Locking the screen"
-  xdg-screensaver lock
+  read otp
+  otplocal=$(oathtool --base32 --totp "$key")
+  if [ "$otp" = "$otplocal" ]; then
+    echo "OTP match locking the screen"
+    xdg-screensaver lock
+    echo "local otp: $otplocal sent otp: $otp"
+  else
+    echo "OTP does not match not locking"
+    echo "local otp: $otplocal sent otp: $otp"
+  fi
 fi
